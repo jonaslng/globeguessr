c48b181c0e8034bb7a15568ae7399cc2c1c09ab7
jonaslng <jonasdelorean@gmail.com>
2025-03-14 18:43:15 +0100

    erstes Grundgerüst

---
+++ src/app/map/page.js
@@ -0,0 +1,45 @@
+"use client";
+var streetview = require('awesome-streetview')
+
+
+export default function Map() {
+
+
+  return (
+    <div>
+      <div>
+        <iframe
+           loading="lazy"
+           referrerPolicy="no-referrer-when-downgrade"
+           src={createURL()}
+           
+           allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture"
+           style={{
+            width: "100vw",
+            height: "calc(100vh + 310px)",
+            zIndex: 100,
+            transform: "translateY(-295px)",
+          }}
+
+        ></iframe>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+let createURL = () => {
+  
+  //THIS IS TO BE EDITED LATER
+  let place = streetview();
+
+  let APIKEY = "AIzaSyAyxozC-4Oof9XzK0pV6tXm5vyukNwLzzA";
+  let lat = place[0];
+  let lon = place[1];
+
+  let url = `https://www.google.com/maps/embed/v1/streetview?key=${APIKEY}&location=${lat},${lon}&heading=210&pitch=10&fov=100&pitch=10&language=en`;
+  return url;
+}
